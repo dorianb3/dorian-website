63b93b0ffc868c79a3a7c279fa8c09e8eaff47e1
dorianbonnet <bonnetdorian9551@gmail.com>
2022-06-04 10:22:46 +0200

    add workflow

--- src/data/index.js
+++ src/data/index.js
@@ -1,6 +1,6 @@
 const test = require('./test_article.json');
 
-const what_is_solana = require('./b0001.json');
+// const what_is_solana = require('./b0001.json');
 const blockchain_consensus_mechanism = require('./b0002.json');
 
 const pyhton_packages = require('./p0001.json');
